656fbae12b18d05f0e1856465f7e70aec9c03760
TheOnlyZac <theonlyzac@gmail.com>
2024-02-08 14:34:41 -0500

    Initial commit

---
+++ canvas.js
@@ -0,0 +1,44 @@
+"use strict";
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+/* Zac's shape */
+// WIP
+
+let x0 = 235;
+let y0 = 415;
+
+let x2 = x0 + 20;
+let y2 = y0 + 20;
+
+let x3 = x2;
+let y3 = y2;
+
+let x4 = x3;
+let y4 = y3;
+
+function drawZac() {
+    /* Blue rect */
+
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.moveTo(x0, y0);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(x3, y3);
+    ctx.lineTo(x4, y4);
+    ctx.fill();
+
+    ctx.fillStyle = "green";
+    ctx.fillRect(235, 415, 20, 50);
+    ctx.fillRect(300, 500, 20, 50);
+}
+
+// The rest of the functions here
+// ...
+
+
+/* Call functions to draw everyone's shapes */
+
+drawZac();
+// ...
