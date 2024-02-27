99093ae00ec45a5c2ccc044cc96bcc81f10863ab
Sylco123 <115595685+Sylco123@users.noreply.github.com>
2024-02-27 13:39:21 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -39,6 +39,17 @@ function drawMoraa(){
 
 
 }
+
+function drawSergio(context, x, y, size) {
+    // Move to a starting point
+    context.translate(x, y); // Move the origin to the specified position
+    context.rotate(Math.PI / 4); // Rotate by 45 degrees
+
+    // Draw a smaller square
+    context.rect(-size / 2, -size / 2, size, size);
+
+    context.stroke();
+}
 // The rest of the functions here
 // ...
 
@@ -63,4 +74,5 @@ function jack() {
 drawZac();
 drawMoraa();
 // ...
+drawSergio();
 jack();
